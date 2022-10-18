c5289c3386d9594f888cc7660ad88a94499a4349
Adejare Faruk <farukpamilerin@yahoo.com>
2022-10-18 04:24:27 -0700

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include  "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+
+ * main - program prints _putchar
+
+ *
+
+ * Return: return 0
+
+ */
+
+int main(void)
+  
+{
+  
+  printf("_putchar\n");
+  
+  return (0);
+  
+}
