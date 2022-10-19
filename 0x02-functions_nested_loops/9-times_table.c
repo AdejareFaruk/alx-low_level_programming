e24b8cd221ff156305f503c384b1cfe315d5417f
AdejareFaruk <113604365+AdejareFaruk@users.noreply.github.com>
2022-10-19 07:04:00 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@
 *add extra space past single digit
 */
 
-void times_table(void)  
+void times_table(void)
 {
 int r, c, d;
 for (r = 0; r <= 9; r++)
@@ -18,18 +18,18 @@ _putchar(' ');
 for (c = 1; c <= 9; c++)
 {
 d = (r * c);
-if ((d / 10) > 0)	    
+if ((d / 10) > 0)
 {
 _putchar((d / 10) + '0');
 }
 else
-{	    
+{
 _putchar(' ');
 }
 _putchar((d % 10) + '0');
 if (c < 9)
 {
-_putchar(',');	      
+_putchar(',');
 _putchar(' ');
 }
 }
